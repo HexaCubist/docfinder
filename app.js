208258415a9272d44dbf5a009a26bf95ca759976
HexaCubist <meta@hexacubist.com>
2017-07-29 11:53:18 +1200

    Continued working on initialization.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var app = angular.module("TrailGen", ["ngRoute"]);
+
+
+
+app.controller('HomeController', function($scope) {
+    $scope.pageClass = "page-welcome";
+});
+
+
 app.config(function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
+    // $locationProvider.html5Mode({
+    //     enabled: true,
+    //     requireBase: false
+    // });
     $routeProvider
-    .when("/welcome", {
-        templateUrl : "views/home.html"
+    .when("/", {
+        templateUrl : "views/home.html",
+        controller: 'HomeController'
     })
-    .otherwise({
-        redirectoTo: '/welcome'
-    })
+    // .otherwise({
+    //     redirectoTo: '/welcome'
+    // })
 });
