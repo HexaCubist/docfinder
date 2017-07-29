bf7bbb157f42801c5d718b3d943c509cf0b51ee5
Heath Grant <flashvolt@gmail.com>
2017-07-30 09:55:28 +1200

    More Updates for sliders

--- convertutils.js
+++ convertutils.js
@@ -1,10 +1,17 @@
 function CaloriesToTimeCal(KPH,Calories,WKG) { //Enternal extar f for function
 
-	var T = Calories/(0.0215*KPH*KPH*KPH-0.1062*KPH*KPH+0.871*KPH)*WKG; //T is Time and this is the Formula to genorate it
+	var T = Calories/((0.0215*KPH*KPH*KPH-0.1765*KPH*KPH+0.871*KPH)*WKG); //T is Time and this is the Formula to genorate it
 
-	T = T/60; // Convertes time from seconds to minutes
+	T = T*60; // Convertes time from hours to minutes
 
 
 	return T; // Writes T
+}
 
+
+function TimeToCaloriesCal(KPH,T,WKG) {
+ //Enternal extar f for function
+var Calories = T*(0.0215*KPH*KPH*KPH-(0.1765*KPH*KPH)+(0.871*KPH)*WKG);
+
+return Calories;
 }
