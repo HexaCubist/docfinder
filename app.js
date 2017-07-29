3393f7aaf5f1735f7657bb4fd7555c2defe03835
HexaCubist <meta@hexacubist.com>
2017-07-30 10:11:22 +1200

    Fixed sliders working off eachother

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.controller('HomeController', function($scope) {
     $scope.Weight = "70";
     $scope.Calories = "70";
     $scope.$watch("Calories", function() {
-        $scope.walklength = Math.round(CaloriesToTimeCal($scope.Speed, $scope.Calories, $scope.Weight));
+        if($($("input[name=Calories]")[0]).is(":focus")) {
+            $scope.walklength = Math.round(CaloriesToTimeCal($scope.Speed, $scope.Calories, $scope.Weight));
+        }
     });
     $scope.$watch("Weight", function() {
         $scope.walklength = Math.round(CaloriesToTimeCal($scope.Speed, $scope.Calories, $scope.Weight));
@@ -17,9 +19,11 @@ app.controller('HomeController', function($scope) {
     $scope.$watch("Speed", function() {
         $scope.walklength = Math.round(CaloriesToTimeCal($scope.Speed, $scope.Calories, $scope.Weight));
     });
-/*    $scope.$watch("walklength", function() {
-       $scope.Calories = TimeToCaloriesCal($scope.Speed, $scope.walklength, $scope.Weight);
-    });*/
+    $scope.$watch("walklength", function() {
+        if($($("input[name=walklength]")[0]).is(":focus")) {
+            $scope.Calories = Math.round(TimeToCaloriesCal($scope.Speed, $scope.walklength, $scope.Weight));
+        }
+    });
 });
 
 app.controller('ResultsController', function($scope, $routeParams) {
--- convertutils.js
+++ convertutils.js
@@ -10,6 +10,7 @@ function CaloriesToTimeCal(KPH,Calories,WKG) { //Enternal extar f for function
 
 
 function TimeToCaloriesCal(KPH,T,WKG) {
+T = T/60; // Convertes time from hours to minutes
  //Enternal extar f for function
 var Calories = T*(0.0215*KPH*KPH*KPH-(0.1765*KPH*KPH)+(0.871*KPH)*WKG);
 
